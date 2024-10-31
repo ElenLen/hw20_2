528774d59af718818718918796ceafeac5210bfa
ElenLen <relax-pl@yandex.ru>
2024-10-31 20:39:27 +0500

    fix: completed hw20_2

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,52 +6,96 @@ function onClick() {
   let selType = type.options[type.selectedIndex].value;
   // см выбранный id сущности
   let idType = document.getElementById("idAtr").value;
-  try {
-    console.log("Выбранная сущность: " + selType);
-    console.log("Выбранный id: " + idType);
-    fetch("http://swapi.py4e.com/api/" + selType + "/" + idType)
-      .then((response) => {
-        return response.json();
-      })
-      .then((user) => {
-        console.log(
-          "Адрес: " + "http://swapi.py4e.com/api/" + selType + "/" + idType
-        );
-        console.log(user);
-        // см какую сущность выбрал пользователь, выводим соотв поля
-        switch (selType) {
-          case "films":
-            document.getElementById("text").innerHTML =
-              "Режиссер: " + user.director;
-            break;
-          case "people":
-            document.getElementById("text").innerHTML = "Имя: " + user.name;
-            break;
-          case "planets":
-            document.getElementById("text").innerHTML =
-              "Название планеты: " + user.name;
-            break;
-          case "species":
-            document.getElementById("text").innerHTML =
-              "Тип персонажа: " + user.name;
-            break;
-          case "starships":
-            document.getElementById("text").innerHTML =
-              "Название космического корабля: " + user.name;
-            break;
-          case "vehicles":
-            document.getElementById("text").innerHTML =
-              "Название ранспортного средства: " + user.name;
-            break;
-        }
-      })
-      .catch((error) => {
-        document.getElementById("text").innerHTML = "Ошибка: " + error;
-        console.log(error);
-      });
-  } catch (error) {
-    console.log("Ошибка: " + error);
-  } finally {
-    console.log("finaly");
+  const load = document.querySelector(".load");
+
+  if (!idType) {
+    document.getElementById("text").innerHTML = "";
+    document.getElementById("result").innerHTML =
+      "Введите id сущности, параметр не заполнен!";
+    return;
   }
+
+  load.classList.remove("progress");
+  console.log("Выбранная сущность: " + selType);
+  console.log("Выбранный id: " + idType);
+
+  fetch("http://swapi.py4e.com/api/" + selType + "/" + idType)
+    .then((response) => {
+      return response.json();
+    })
+    .then((user) => {
+      // try {
+      console.log(user);
+      document.getElementById("text").innerHTML = "";
+      document.getElementById("result").innerHTML = "";
+      // см какую сущность выбрал пользователь, выводим соотв поля
+      switch (selType) {
+        case "films":
+          if (!user.director) {
+            document.getElementById("result").innerHTML = "Не найден!";
+          } else {
+            document.getElementById(
+              "text"
+            ).innerHTML = `Режиссер: ${user.director} <br><br> Название фильма: ${user.title}`;
+          }
+          break;
+        case "people":
+          if (!user.name) {
+            document.getElementById("result").innerHTML = "Не найден!";
+            return;
+          } else {
+            document.getElementById(
+              "text"
+            ).innerHTML = `Имя персонажа: ${user.name} <br><br> Рост персонажа: ${user.height}`;
+          }
+          break;
+        case "planets":
+          if (!user.name) {
+            document.getElementById("result").innerHTML = "Не найден!";
+            return;
+          } else {
+            document.getElementById(
+              "text"
+            ).innerHTML = `Название планеты: ${user.name} <br><br> Диаметр планеты: ${user.diameter}`;
+          }
+          break;
+        case "species":
+          if (!user.name) {
+            document.getElementById("result").innerHTML = "Не найден!";
+            return;
+          } else {
+            document.getElementById(
+              "text"
+            ).innerHTML = `Название вида персонажа: ${user.name} <br><br> Классификация вида персонажа: ${user.classification}`;
+          }
+          break;
+        case "starships":
+          if (!user.name) {
+            document.getElementById("result").innerHTML = "Не найден!";
+            return;
+          } else {
+            document.getElementById(
+              "text"
+            ).innerHTML = `Название космического корабля: ${user.name} <br><br> Модель коробля: ${user.model}`;
+          }
+          break;
+        case "vehicles":
+          if (!user.name) {
+            document.getElementById("result").innerHTML = "Не найден!";
+            return;
+          } else {
+            document.getElementById(
+              "text"
+            ).innerHTML = `Название транспорта: ${user.name}  <br><br> Модель: ${user.model}`;
+          }
+          break;
+        default:
+          document.getElementById("result").innerHTML = "Не найден!";
+      }
+    })
+    .catch((error) => {
+      document.getElementById("result").innerHTML = "Ошибка: " + error;
+      console.log(error);
+    })
+    .finally(() => load.classList.add("progress"));
 }
